3b705bf4d320c787917da07cdf786cc4d60195e3
simdhlu022 <simdhlu022@student.wethinkcode.co.za>
2023-07-08 01:47:08 +0200

    first commit

---
+++ airline_reservation_system.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+void mainMenu();
+
+class Management
+{
+    public:
+        Management()
+        {
+            mainMenu();a
+        }
+};
+
+
+void mainMenu()
+{
+    int lchoice;
+    int schoice;
+    int back;
+
+    cout << "\t                 United Airlines \n" << endl;
+    cout << "\t_________________Main Menu_________________" << endl;
+
+    cout << "\t____________________________________________" << endl;
+
+
+}
+
+int main()
+{
+    Management Mobj;
+    return 0;
+}
